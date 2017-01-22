ae2e54269a177fe8317dd9e1a2cdef11bcfcb1c5
zsyzzx <zsyzzx@foxmail.com>
2017-01-22 19:02:05 +0800

    begin assignments in weeks queue

---
+++ src/part1/week2/assignment/Deque.java
@@ -0,0 +1,40 @@
+package part1.week2.assignment;
+
+import java.util.Iterator;
+
+/**
+ * Created by seven on 2017/1/22.
+ */
+public class Deque<Item> implements Iterable<Item> {
+    private int count;
+
+    public Deque() {
+    }
+
+    public int size(){
+        return count;
+    }
+
+    public void addFirst(Item item) {
+
+    }
+    public void addLast(Item item) {
+
+    }
+
+    public Item removeFirst(Item item) {
+        Item temp;
+
+        return temp;
+    }
+    public Item removeLast(Item item) {
+        Item temp;
+
+        return temp;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
---
+++ src/part1/week2/assignment/RandomizedQueue.java
@@ -0,0 +1,13 @@
+package part1.week2.assignment;
+
+import java.util.Iterator;
+
+/**
+ * Created by seven on 2017/1/22.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
