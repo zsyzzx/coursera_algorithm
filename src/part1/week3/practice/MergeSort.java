223fea9dadf623f030336914aa0deafc0541a800
seven <zsyzzx@foxmail.com>
2017-08-14 18:04:41 +0800

    实现了Mergesort的优化算法

--- src/part1/week3/practice/MergeSort.java
+++ src/part1/week3/practice/MergeSort.java
@@ -1,8 +1,11 @@
 package part1.week3.practice;
 
+import edu.princeton.cs.algs4.Merge;
+import edu.princeton.cs.algs4.MergeBU;
 import edu.princeton.cs.algs4.StdOut;
 import part1.week2.practice.Insertion;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
@@ -55,17 +58,81 @@ public class MergeSort {
 
     public static final int CUTOFF = 7;
 
-    private static void mergeX_merge(Comparable[] src, Comparable[] dest, int lo, int hi) {
+    private static void mergeX_merge(Comparable[] src, Comparable[] dest, int lo, int mid, int hi) {
+        assert isSorted(src, lo, mid);
+        assert isSorted(src, mid + 1, hi);
 
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) dest[k] = src[j++];
+            else if (j > hi) dest[k] = src[i++];
+            else if (less(src[j], src[i])) dest[k] = src[j++];
+            else dest[k] = src[i++];
+        }
+    }
 
+    private static void mergeX_sort(Comparable[] src, Comparable[] dest, int lo, int hi) {
+        if (hi <= lo + CUTOFF) {
+            Insertion.sort(dest, lo, hi);
+            return;
+        }
+        int mid = (lo + hi) >>> 1;
+//        将中间的排序结果放在src中，可以直接从src开始合并
+        mergeX_sort(dest, src, lo, mid);
+        mergeX_sort(dest, src, mid + 1, hi);
+        if (!less(src[mid + 1], src[mid])) {
+            System.arraycopy(src, lo, dest, lo, hi - lo + 1);
+        }
+        mergeX_merge(src,dest,lo,mid,hi);
+    }
+
+    public static void mergeX_sort(Comparable[] a){
+        Comparable[] aux = a.clone();
+        mergeX_sort(aux,a,0,a.length-1);
+        assert isSorted(a);
     }
 
 
+//    Bottom-Up merge sort
+//    无递归合并排序
+//    和原来的merge一样
+    private static void mergeBU_merge(Comparable[] a,Comparable[] aux ,int lo,int mid,int hi){
+        for (int k = lo; k<=hi;k++){
+            aux[k] = a[k];
+
+        }
+
+    }
+
+    // TODO: 2017/8/14 阅读源码，理解源码
+    public static void mergeBU_sort(Comparable[] a){
+        int n = a.length;
+        Comparable[] aux = new Comparable[n];
+        for (int len=1;len<n;len*=2){
+//            len代表每一周期里合并，排序的值的对数
+            for (int lo = 0; lo<n-len;lo+=len+len){
+                int mid = lo+len-1;
+                int hi = Math.min(lo+len+len-1,n-1);
+                merge(a,aux,lo,mid,hi);
+            }
+        }
+    }
+
+    // TODO: 2017/8/14 看inversion的代码
+    private void testInversions(){
+        String[] a = {"a","b","I"};
+        Inversions.count(a);
+
+    }
 
     public static void main(String[] args) {
         String[] strings = {"A", "B", "a", "d", "e", "Z", "y", "K", "I"};
-        assert isSorted(strings);
-        MergeSort.sort(strings);
+//        assert isSorted(strings);
+//        MergeSort.sort(strings);
+        show(strings);
+        System.out.println();
+//        MergeSort.mergeX_sort(strings);
+        MergeSort.mergeBU_sort(strings);
         assert isSorted(strings);
         show(strings);
     }
